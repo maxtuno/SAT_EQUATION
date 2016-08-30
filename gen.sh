c40722a3540f916ea4acf459065a5afd961745c5
O. A. Riveros <oscar.riveros@gmail.com>
2016-08-30 12:01:57 -0300

    Update

--- gen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# pip install cnfgen
-# <k> <n> <m>
-# Note: <k> = <n>
-# Need Edit Comments
-# k = n % m = 2^m => unsatisfiable
-cnfgen randkcnf 11 11 2048 > cnf_polynomial_unsat.abs
-cnfgen randkcnf 11 11 2047 > cnf_polynomial_sat.abs
